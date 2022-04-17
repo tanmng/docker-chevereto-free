dbdea8b94d844657d255cfa8215a853ae7f212a1
Manh Tan Nguyen <tan.mng90@gmail.com>
2022-04-17 14:58:46 -0400

    Correct missing char in entrypoint script

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -5,11 +5,11 @@
 case "${CHEVERETO_VERSION}" in
     1.6.*|latest)
         # Display message to tell user to upgrade to latest env-var structure if needed
-        if [ -z "${CHEVERETO_DB_USER}"  && ! -z "${CHEVERETO_DB_USERNAME}" ]; then
+        if [ -z "${CHEVERETO_DB_USER}" ] && [ ! -z "${CHEVERETO_DB_USERNAME}" ]; then
             echo "From version 1.6.1 Chevereto free use env-var CHEVERETO_DB_USER to specify the database username instead of CHEVERETO_DB_USERNAME, please consider updating your Docker container config to user the correct env-var"
             export CHEVERETO_DB_USER="${CHEVERETO_DB_USERNAME}"
         fi
-        if [ -z "${CHEVERETO_DB_PASS}"  && ! -z "${CHEVERETO_DB_PASSWORD}" ]; then
+        if [ -z "${CHEVERETO_DB_PASS}" ] && [ ! -z "${CHEVERETO_DB_PASSWORD}" ]; then
             echo "From version 1.6.1 Chevereto free use env-var CHEVERETO_DB_PASS to specify the database password instead of CHEVERETO_DB_PASSWORD, please consider updating your Docker container config to user the correct env-var"
             export CHEVERETO_DB_PASS="${CHEVERETO_DB_PASSWORD}"
         fi
